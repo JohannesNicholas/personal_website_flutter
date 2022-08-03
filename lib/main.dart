368a0221dbacde2b5d9ee2985b18f3c4b94d414b
JohannesNicholas <goanna7007@gmail.com>
2022-08-03 22:45:10 +1000

    Changed how drawer is stored and loaded to wor

--- lib/main.dart
+++ lib/main.dart
@@ -62,6 +62,9 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  int page = 0;
+
+  var pages = [];
 
   void _incrementCounter() {
     setState(() {
@@ -82,52 +85,59 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
+
+    var drawer = Drawer(
+      child: ListView(
+        children: <Widget>[
+          DrawerHeader(
+            child: Text('Johannes Nicholas Portfolio'),
+            decoration: BoxDecoration(
+              color: Colors.orange,
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.home),
+            title: Text('Home'),
+            onTap: () {
+              page = 1;
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.work),
+            title: Text('My work'),
+            onTap: () {
+              page = 2;
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.share),
+            title: Text('Socials'),
+            onTap: () {
+              page = 3;
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.email),
+            title: Text('Contact'),
+            onTap: () {
+              page = 4;
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      ),
+    );
+
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      drawer: Drawer(
-        child: ListView(
-          children: <Widget>[
-            DrawerHeader(
-              child: Text('Johannes Nicholas Portfolio'),
-              decoration: BoxDecoration(
-                color: Colors.orange,
-              ),
-            ),
-            ListTile(
-              leading: Icon(Icons.home),
-              title: Text('Home'),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.work),
-              title: Text('My work'),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.share),
-              title: Text('Socials'),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.email),
-              title: Text('Contact'),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-          ],
-        ),
-      ),
+      drawer: drawer,
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
